9d4f4404242d1202314b5d504441fa6a38d46961
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-12 16:53:05 +0700

    add username prop

--- BE/VibeTunes.Domain/Entities/User.cs
+++ BE/VibeTunes.Domain/Entities/User.cs
@@ -5,6 +5,7 @@ namespace VibeTunes.Domain.Entities;
 
 public class User : BaseEntity
 {
+    public string Username { get; set; }
     public string Password {get; set;}
     public Email EmailAddress {get; set;}
     public bool IsActive { get; set; } = false;
@@ -12,8 +13,9 @@ public class User : BaseEntity
     
     private User() {}
 
-    public User(string password, Email emailAddress)
+    public User(string username, string password, Email emailAddress)
     {
+        Username = username;
         Password = password;
         EmailAddress = emailAddress;
     }
