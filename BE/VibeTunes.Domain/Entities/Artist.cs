131a70cc363dd37a41b0501a8e7cfe2daa836eed
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-12 17:00:11 +0700

    update listener and artist classes inherit from user

---
+++ BE/VibeTunes.Domain/Entities/Artist.cs
@@ -0,0 +1,10 @@
+﻿using VibeTunes.Domain.ValueObjects;
+
+namespace VibeTunes.Domain.Entities;
+
+public class Artist(string userName, string password, Email email, string stageName, string bio)
+    : User(userName, password, email)
+{
+    public string StageName { get; private set; } = stageName;
+    public string Bio { get; private set; } = bio;
+}
---
+++ BE/VibeTunes.Domain/Entities/Listener.cs
@@ -0,0 +1,5 @@
+﻿using VibeTunes.Domain.ValueObjects;
+
+namespace VibeTunes.Domain.Entities;
+
+public class Listener(string username, string password, Email emailAddress) : User(username, password, emailAddress);
