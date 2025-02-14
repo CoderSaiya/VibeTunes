81b7b215708ab05ae16eff7b1ee04299a30e8ea5
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-14 16:00:28 +0700

    fix(api): init web api

--- BE/VibeTunes.API/Program.cs
+++ BE/VibeTunes.API/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Service to container DI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Cấu hình pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.Run();
