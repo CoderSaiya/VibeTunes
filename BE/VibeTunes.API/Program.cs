3497ccd2c6c3d12c1b59ba8a88996f28713398ca
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-15 11:39:42 +0700

    fix(api): add scope for sql server

--- BE/VibeTunes.API/Program.cs
+++ BE/VibeTunes.API/Program.cs
@@ -1,12 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using VibeTunes.Infrastructure.Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Service to container DI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure Entity Framework
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(
+            connectionString: "Data Source=DESKTOP-1FAVEMH\\SQLEXPRESS;Initial Catalog=VibeTunesDb;Integrated Security=True;trusted_connection=true;encrypt=false;",
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            }),
+    contextLifetime: ServiceLifetime.Scoped,
+    optionsLifetime: ServiceLifetime.Singleton
+);
+
+
 var app = builder.Build();
 
-// Cấu hình pipeline HTTP
+// Config pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
