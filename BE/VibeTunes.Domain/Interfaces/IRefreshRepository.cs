77ba47416da1d94d5e1afc5defdef25e3e5278f5
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-15 21:34:41 +0700

    feat(domain,infra): add refresh token repo

---
+++ BE/VibeTunes.Domain/Interfaces/IRefreshRepository.cs
@@ -0,0 +1,10 @@
+﻿using VibeTunes.Domain.Entities;
+
+namespace VibeTunes.Domain.Interfaces;
+
+public interface IRefreshRepository
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+    Task AddAsync(RefreshToken refreshToken);
+    Task UpdateAsync(RefreshToken refreshToken);
+}
---
+++ BE/VibeTunes.Infrastructure/Persistence/Repositories/RefreshRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VibeTunes.Domain.Entities;
+using VibeTunes.Domain.Interfaces;
+using VibeTunes.Infrastructure.Persistence.Data;
+
+namespace VibeTunes.Infrastructure.Persistence.Repositories;
+
+public class RefreshRepository : IRefreshRepository
+{
+    private readonly AppDbContext _context;
+
+    public RefreshRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        return await _context.RefreshTokens
+            .Where(t => t.Token == token)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken)
+    {
+        await _context.RefreshTokens.AddAsync(refreshToken);
+    }
+
+    public async Task UpdateAsync(RefreshToken refreshToken)
+    {
+        var token = await _context.RefreshTokens.FindAsync(refreshToken.Id);
+        if (token == null)
+        {
+            throw new KeyNotFoundException();
+        }
+        _context.Entry(token).CurrentValues.SetValues(refreshToken);
+    }
+}
