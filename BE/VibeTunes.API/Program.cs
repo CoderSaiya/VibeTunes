5f9a781eef57694092020c9a47b8626cac35aa23
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-15 13:48:42 +0700

    fix(api): config swagger with jwt bearer

--- BE/VibeTunes.API/Program.cs
+++ BE/VibeTunes.API/Program.cs
@@ -1,10 +1,38 @@
+using Microsoft.OpenApi.Models;
 using VibeTunes.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Service to container DI
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VibeTunes", Version = "v1" }));
+
+builder.Services.AddSwaggerGen(c =>
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    }));
+builder.Services.AddSwaggerGen(c =>
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    }));
 
 // Configure Dependency Injection
 builder.Services.AddInfrastructure(builder.Configuration);
