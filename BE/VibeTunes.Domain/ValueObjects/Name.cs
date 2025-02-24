7c1f93db827d6131fbd54809fefd77540c78e10a
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-24 13:27:25 +0700

    fix(domain): format name

--- BE/VibeTunes.Domain/ValueObjects/Name.cs
+++ BE/VibeTunes.Domain/ValueObjects/Name.cs
@@ -2,8 +2,8 @@
 
 public class Name
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; }
+    public string LastName { get; }
 
     public Name(string firstName, string lastName)
     {
