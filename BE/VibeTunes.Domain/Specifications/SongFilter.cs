ca89736b4655ab284d94458911bc71697211e455
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-24 13:12:00 +0700

    feat(domain): add support for parallel filter specifications

---
+++ BE/VibeTunes.Domain/Specifications/SongFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace VibeTunes.Domain.Specifications;
+
+public class SongFilter
+{
+    public string? TitleContains { get; init; }
+    public string? Genre { get; init; }
+    public TimeSpan? MinDuration { get; init; }
+    public TimeSpan? MaxDuration { get; init; }
+    public DateTime? ReleaseAfter { get; init; }
+    public DateTime? ReleaseBefore { get; init; }
+    public string SortBy { get; init; } = "Title";
+    public string SortDirection { get; init; } = "asc";
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
