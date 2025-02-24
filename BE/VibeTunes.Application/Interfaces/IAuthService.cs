9171d9b05c26929c0454e974bd2b729b4859b99c
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-24 13:41:39 +0700

    feat(app, infra): define interface and implement auth service

---
+++ BE/VibeTunes.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using VibeTunes.Domain.Entities;
+
+namespace VibeTunes.Application.Interfaces;
+
+public interface IAuthService
+{
+    public string HashPassword(string password);
+    bool VerifyPassword(string password, string hashedPassword);
+    string GenerateAccessToken(IEnumerable<Claim> claims);
+    string GenerateRefreshToken();
+}
---
+++ BE/VibeTunes.Infrastructure/Identity/AuthService.cs
@@ -0,0 +1,51 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using VibeTunes.Application.Interfaces;
+using VibeTunes.Domain.Entities;
+
+namespace VibeTunes.Infrastructure.Identity;
+
+public class AuthService(IConfiguration configuration) : IAuthService
+{
+
+    public string HashPassword(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+        return Convert.ToBase64String(bytes);
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        return HashPassword(password) == hashedPassword;
+    }
+
+    public string GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["SecurityKey"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["Issuer"],
+            audience: configuration["Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+        
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new Byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+}
