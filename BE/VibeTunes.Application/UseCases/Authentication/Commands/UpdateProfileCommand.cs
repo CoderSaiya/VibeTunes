ef60eed1ebb98786044e9a11f23d179388fa515b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-27 10:44:00 +0700

    feat(app): handle update profile event

---
+++ BE/VibeTunes.Application/UseCases/Authentication/Commands/UpdateProfileCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using VibeTunes.Domain.ValueObjects;
+
+namespace VibeTunes.Application.UseCases.Authentication.Commands;
+
+public record UpdateProfileCommand(
+    Guid UserId,
+    string? FirstName,
+    string? LastName,
+    string? Address,
+    string? Gender,
+    string? Avatar
+    ) : IRequest<Unit>;
---
+++ BE/VibeTunes.Application/UseCases/Authentication/Events/UpdateProfileHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using VibeTunes.Application.Exceptions;
+using VibeTunes.Application.UseCases.Authentication.Commands;
+using VibeTunes.Domain.Interfaces;
+using VibeTunes.Domain.ValueObjects;
+
+namespace VibeTunes.Application.UseCases.Authentication.Events;
+
+public class UpdateProfileHandler(
+    IUserRepository userRepository, 
+    IUnitOfWork unitOfWork
+    ) : IRequestHandler<UpdateProfileCommand, Unit>
+{
+    public async Task<Unit> Handle(UpdateProfileCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdAsync(request.UserId);
+        if (user is null) 
+            throw new BusinessException("User does not exist");
+        
+        var profile = user.Profile;
+
+        if (!string.IsNullOrWhiteSpace(request.FirstName) || !string.IsNullOrWhiteSpace(request.LastName))
+            profile.Name = new Name(request.FirstName ?? profile.Name?.FirstName, 
+                request.LastName ?? profile.Name?.LastName);
+
+        if (!string.IsNullOrWhiteSpace(request.Address))
+            profile.Address = Address.Parse(request.Address);
+
+        if (!string.IsNullOrWhiteSpace(request.Gender) && Enum.TryParse<Gender>(request.Gender, true, out var gender))
+            profile.Gender = gender;
+
+        if (!string.IsNullOrWhiteSpace(request.Avatar))
+            profile.Avatar = request.Avatar;
+        
+        await unitOfWork.CommitAsync();
+        
+        return Unit.Value;
+    }
+}
