2cdc346bb53cc7feb161bedc22637fdcf2ea43ec
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-27 10:38:43 +0700

    feat(app): handle change password event

---
+++ BE/VibeTunes.Application/UseCases/Authentication/Commands/ChangePasswordCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace VibeTunes.Application.UseCases.Authentication.Commands;
+
+public record ChangePasswordCommand(
+    Guid UserId,
+    string OldPassword,
+    string NewPassword
+    ) : IRequest<string>;
---
+++ BE/VibeTunes.Application/UseCases/Authentication/Events/ChangePasswordHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using VibeTunes.Application.Exceptions;
+using VibeTunes.Application.Interfaces;
+using VibeTunes.Application.UseCases.Authentication.Commands;
+using VibeTunes.Domain.Interfaces;
+
+namespace VibeTunes.Application.UseCases.Authentication.Events;
+
+public class ChangePasswordHandler(
+    IAuthService authService,
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<ChangePasswordCommand, string>
+{
+    public async Task<string> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+    {
+        //check user exist
+        var existingUser = await userRepository.GetByIdAsync(request.UserId);
+        if (existingUser == null)
+            throw new BusinessException("User does not exist");
+        
+        // check old pass
+        if (!authService.VerifyPassword(request.OldPassword, existingUser.Password))
+            throw new BusinessException("Invalid password.");
+        
+        // change pass
+        var newPass = authService.HashPassword(request.NewPassword);
+        existingUser.Password = newPass;
+        
+        await unitOfWork.CommitAsync();
+        
+        return newPass;
+    }
+}
