ac134cc86fb266c12e2d252edaae16dd23aa5fa3
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-03 10:31:23 +0700

    feat(domain): define aws option

---
+++ BE/VibeTunes.Domain/Common/AWSOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace VibeTunes.Domain.Common;
+
+public sealed class AWSOptions
+{
+    public string Profile { get; set; }
+    public string Region { get; set; }
+    public string BucketName { get; set; }
+}
