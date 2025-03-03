da261f800fbc63ffb9348e92f1e73f6a40d055f1
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-03 19:00:27 +0700

    feat(app): handle create a song event

---
+++ BE/VibeTunes.Application/UseCases/Songs/Commands/CreateSongCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace VibeTunes.Application.UseCases.Songs.Commands;
+
+public record CreateSongCommand(
+    Guid ArtistId,
+    Guid? AlbumId,
+    string Title,
+    IFormFile Image,
+    IFormFile Audio,
+    DateTime ReleaseDate,
+    string Genre
+    ) : IRequest<Guid>;
---
+++ BE/VibeTunes.Application/UseCases/Songs/Events/CreateSongHandler.cs
@@ -0,0 +1,76 @@
+﻿using MediatR;
+using VibeTunes.Application.Exceptions;
+using VibeTunes.Application.Interfaces;
+using VibeTunes.Application.UseCases.Songs.Commands;
+using VibeTunes.Domain.Entities;
+using VibeTunes.Domain.Interfaces;
+
+namespace VibeTunes.Application.UseCases.Songs.Events;
+
+public class CreateSongHandler(
+    ISongRepository songRepository,
+    IUserRepository userRepository,
+    IGenreRepository genreRepository,
+    INotificationService notificationService,
+    IFileService fileService,
+    IUnitOfWork unitOfWork
+    ) : IRequestHandler<CreateSongCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateSongCommand request, CancellationToken cancellationToken)
+    {
+        // check artist exist
+        var existingArtist = await userRepository.GetByIdAsync(request.ArtistId);
+        if (existingArtist is null)
+            throw new BusinessException("Artist not found!");
+        
+        // upload audio & image
+        var audioKey = $"audios/{existingArtist.Id}/{Guid.NewGuid()}_{request.Audio.FileName}";
+        await using (var audioStream = request.Audio.OpenReadStream())
+        {
+            await fileService.UploadFileAsync(audioStream, audioKey, cancellationToken);
+        }
+        
+        var imageKey = $"images/{existingArtist.Id}/{Guid.NewGuid()}_{request.Image.FileName}";
+        await using (var imageStream = request.Image.OpenReadStream())
+        {
+            await fileService.UploadFileAsync(imageStream, imageKey, cancellationToken);
+        }
+        
+        // create song
+        var genreNames = request.Genre
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Distinct()
+            .ToList();
+        
+        var existingGenres = await genreRepository.GetGenreByNameAsync(genreNames);
+        if (existingGenres.Count != genreNames.Count)
+        {
+            var invalidGenres = genreNames.Except(existingGenres.Select(g => g.GenreName)).ToList();
+            throw new BusinessException($"Invalid genres: {string.Join(", ", invalidGenres)}");
+        }
+        
+        var newSong = new Song
+        {
+            ArtistId = existingArtist.Id,
+            AlbumId = request.AlbumId,
+            Title = request.Title,
+            CoverImgUrl = imageKey,
+            FileUrl = audioKey,
+            ReleaseDate = request.ReleaseDate,
+            Genres = existingGenres,
+        };
+        
+        await songRepository.AddSongAsync(newSong);
+        
+        // send notification to artist
+        await notificationService.SendNotification(
+            "admin",
+            existingArtist.Username,
+            $"Your new song created: {newSong.Title}"
+            );
+        
+        await unitOfWork.CommitAsync();
+        
+        return newSong.Id;
+    }
+}
