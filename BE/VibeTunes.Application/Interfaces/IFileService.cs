0cbf363a5d6ecd90db5f8df4843745024ea3a538
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-03 10:35:57 +0700

    feat(app): define 3rd party interface

---
+++ BE/VibeTunes.Application/Interfaces/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace VibeTunes.Application.Interfaces;
+
+public interface IFileService
+{
+    public Task UploadFileAsync(Stream fileStream, string fileName, CancellationToken cancellationToken);
+    Task<Stream> DownloadFileAsync(string fileName, CancellationToken cancellationToken);
+}
---
+++ BE/VibeTunes.Application/Interfaces/IGmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace VibeTunes.Application.Interfaces;
+
+public interface IGmailService
+{
+    public Task SendEmailAsync(string toEmail, string subject, string body);
+}
