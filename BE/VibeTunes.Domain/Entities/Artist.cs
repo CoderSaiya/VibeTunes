bd509d3b370615e5c21adcd5915e415e57e3318b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-05 18:25:59 +0700

    fix(domain): change to public set prop

--- BE/VibeTunes.Domain/Entities/Artist.cs
+++ BE/VibeTunes.Domain/Entities/Artist.cs
@@ -4,8 +4,8 @@ namespace VibeTunes.Domain.Entities;
 
 public class Artist : User
 {
-    public string StageName { get; private set; }
-    public string Bio { get; private set; }
+    public string StageName { get; set; }
+    public string Bio { get; set; }
     
     public ICollection<Album> Albums { get; set; } = new List<Album>();
     public ICollection<Song> Songs { get; set; } = new List<Song>();
