761baad13a4109a4d21d84b090e613ec5b70acc4
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-14 08:13:49 +0700

    feat(app): handle get users event

---
+++ BE/VibeTunes.Application/UseCases/Users/Events/GetUsersEvent.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using VibeTunes.Application.DTOs;
+using VibeTunes.Application.UseCases.Users.Queries;
+using VibeTunes.Domain.Interfaces;
+using VibeTunes.Domain.Specifications;
+
+namespace VibeTunes.Application.UseCases.Users.Events;
+
+public class GetUsersEvent(
+    IUserRepository userRepository
+    ) : IRequestHandler<GetUsersQuery, IEnumerable<UserDto>>
+{
+    public async Task<IEnumerable<UserDto>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.Filter ?? new UserFilter();
+        var users = await userRepository.GetUsersByFilterAsync(filter);
+        return users.Select(u => new UserDto(
+            u.Username, 
+            u.Password,
+            u.Profile.Name!.FirstName, 
+            u.Profile.Name.LastName, 
+            u.Rank.ToString(), 
+            u.IsActive, 
+            u.IsBanned, 
+            u.Profile.Address!,
+            u.Profile.Gender.ToString() ?? null, 
+            u.Profile.Avatar!)
+        );
+    }
+}
---
+++ BE/VibeTunes.Application/UseCases/Users/Queries/GetUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using VibeTunes.Application.DTOs;
+using VibeTunes.Domain.Specifications;
+
+namespace VibeTunes.Application.UseCases.Users.Queries;
+
+public sealed record GetUsersQuery(UserFilter? Filter) : IRequest<IEnumerable<UserDto>>;
