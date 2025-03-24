5ce40d5a43e14635939e15df23e085cf809681b8
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-03-24 11:26:05 +0700

    feat(app): implement user event history export

---
+++ BE/VibeTunes.Application/UseCases/Histories/Events/ExportUserHistoryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using MediatR;
+using VibeTunes.Application.Interfaces;
+using VibeTunes.Application.UseCases.Histories.Commands;
+using VibeTunes.Domain.Common;
+using VibeTunes.Domain.Interfaces;
+
+namespace VibeTunes.Application.UseCases.Histories.Events;
+
+public class ExportUserHistoryHandler(
+    IHistoryRepository historyRepository,
+    IFileService fileService
+    ) : IRequestHandler<ExportUserHistoryCommand, string>
+{
+    public async Task<string> Handle(ExportUserHistoryCommand request, CancellationToken cancellationToken)
+    {
+        // get histories list
+        var histories = await historyRepository.GetAllHistoriesAsync();
+        
+        // export and storage csv file (string builder)
+        var sb = new StringBuilder();
+        
+        sb.AppendLine("UserId, SongId, ListenDuration");
+
+        foreach (var history in histories)
+            sb.AppendLine($"{GuidConverter.ToFloat(history.UserId)}," + 
+                          $"{GuidConverter.ToFloat(history.SongId)}," + 
+                          $"{history.ListenDuration}");
+
+        var csvBytes = Encoding.UTF8.GetBytes(sb.ToString());
+        
+        var fileName = $"song-rating.csv";
+        
+        var filePath = await fileService.SaveFileAsync(csvBytes, fileName, null!, cancellationToken);
+        
+        return filePath;
+    }
+}
